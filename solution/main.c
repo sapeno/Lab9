abe18e4b3340536f16717dfcebdb0745d983a6f0
alina <alinashemeneeva@gmail.com>
2025-11-28 16:34:31 +0300

    Work done

--- solution/main.c
+++ solution/main.c
@@ -39,7 +39,7 @@ int main() {
     int l = -3;
 
     int k = 0;
-    while (k < 50 && !isPointInEllipse(&p, &e)) {
+    while (k <= 50 && !isPointInEllipse(&p, &e)) {
         printf("k = %d, i = %d, j = %d\n", k, p.i, p.j);
         struct Point p0 = p;
         p.i = mod(abs((p0.i + k) * (p0.j + 2*k) * (l + 3*k)), 35);
